5430e8f006d8556225d1662e0cc0ef5d1fb0b5af
erenmeric <programmerenmeric@gmail.com>
2025-05-13 21:19:05 +0300

    içerik temizleme

--- src/main/java/com/example/massproject/repository/ComplaintRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.massproject.repository;
-
-import com.example.massproject.model.ComplaintEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComplaintRepository extends JpaRepository<ComplaintEntity, String> {
-}
