507377b862a886705512ffc45c2047e854f97db0
programmeren <programmerenmeric@gmail.com>
2025-05-13 11:58:04 +0300

    Add files via upload

---
+++ massproject-2/src/main/java/com/example/massproject/MassProjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.massproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MassProjectApplication.class, args);
+    }
+}
